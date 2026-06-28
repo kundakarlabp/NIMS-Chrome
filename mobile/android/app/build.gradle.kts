3c899b1ec791f27d8e4f139c9ebfc182158f8009
Kundakarlabp <kundakarlabp@gmail.com>
2026-06-28 21:20:46 +0530

    build(android): release on-demand WebView as 0.10.0

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "org.kundakarlab.nimsfastsummarymobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 24
-        versionName = "0.9.1"
+        versionCode = 25
+        versionName = "0.10.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
