ef3658fb1078e5a14b6896df5695b5220b874b6b
Kundakarlabp <kundakarlabp@gmail.com>
2026-08-02 05:16:22 +0530

    Release Android 0.10.4

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.kundakarlab.nimsfastsummarymobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 40
-        versionName = "0.10.3"
+        versionCode = 41
+        versionName = "0.10.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures {
